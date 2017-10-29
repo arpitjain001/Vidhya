8fa61ba7fb379ad65ed63bf5120b1f3bc62ef0d2
Arpit Jain <arpitjain001@hotmail.com>
2017-10-29 16:23:26 +0530

    Insert temp data script

---
+++ DBScripts/InsertData.sql
@@ -0,0 +1,9 @@
+﻿Insert into [Config].[GenderSet] (GenderCode, GenderDescription) Values ('M', 'Male')
+Insert into [Config].[GenderSet] (GenderCode, GenderDescription) Values ('F', 'Female')
+Insert into [Config].[GenderSet] (GenderCode, GenderDescription) Values ('O', 'Other')
+
+Insert into [Config].[MaritalStatus] (MaritalStatusCode, MaritalStatusDescription) Values ('MAR', 'Married')
+Insert into [Config].[MaritalStatus] (MaritalStatusCode, MaritalStatusDescription) Values ('SE', 'Separated')
+Insert into [Config].[MaritalStatus] (MaritalStatusCode, MaritalStatusDescription) Values ('DV', 'Divorced')
+Insert into [Config].[MaritalStatus] (MaritalStatusCode, MaritalStatusDescription) Values ('WD', 'Widowed')
+Insert into [Config].[MaritalStatus] (MaritalStatusCode, MaritalStatusDescription) Values ('UNMAR', 'Never Married')
